e212f68a730bd8921d2df6d8764e6ac3c11f4715
sergeyzharko <sergeyzharko@gmail.com>
2018-02-22 02:20:17 +0300

    fixes

--- app/scripts/sources.js
+++ app/scripts/sources.js
@@ -1,24 +1,5 @@
-//import loadNews from './news';
-
-const sources = [
-    {
-        name: 'Google News',
-        logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Google_News_Logo.svg/1200px-Google_News_Logo.svg.png',
-        link: 'google-news-ru'
-    },
-    {
-        name: 'BBC News',
-        logo: 'http://m.files.bbci.co.uk/modules/bbc-morph-news-waf-page-meta/1.2.0/bbc_news_logo.png',
-        link: 'bbc-news'
-    },
-    {
-        name: 'РБК',
-        logo: 'http://yesjob.ru/wp-content/uploads/2016/08/1363630871.png',
-        link: 'rbc'
-    }
-];
-
-
+import '../assets/sources.json';
+var sources;
 var url = 'https://newsapi.org/v2/sources?' +
 'apiKey=f53b56a81c57478689c3058487c41269';
 
@@ -35,12 +16,19 @@ async function readSourceArray(){
     return await sources;
 }
 
-
-
 export default function loadSources() {
-    let buttons = document.querySelector('.buttons');
 
-    readSourceArray().then(
+    fetch('./assets/sources.json')
+    .then(function(response) {
+      console.log(response.headers.get('Content-Type')); // application/json; charset=utf-8
+      console.log(response.status); // 200
+    
+      return response.json();
+     })
+    .then(function(res) {
+      sources = res;
+      let buttons = document.querySelector('.buttons');
+      readSourceArray().then(
         (sources => {
             sources.forEach( source => {
                 let button = document.createElement('div');
@@ -49,7 +37,7 @@ export default function loadSources() {
                 button.innerHTML = `
                     <img class="source-image" src="${source.logo}" alt="${source.name}">
                     <a href="#"><div class="picture lion"></div></a>`;
-               // button.addEventListener('click', () => loadNews(source.link) );
+                // button.addEventListener('click', () => loadNews(source.link) );
                 button.addEventListener('click', () => import(/* webpackChunkName: "news" */ './news').then(module => {
                         var loadNews = module.default;
                         loadNews(source.link);
@@ -58,6 +46,9 @@ export default function loadSources() {
                 buttons.appendChild(button);
             });
         }) 
-    )
+      )
+    })
+    .catch( console.log );
+
 }
 
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,107 +2,145 @@ const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-var package = require('../package.json');
+const autoprefixer = require('autoprefixer');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const package = require('../package.json');
+const filelist = require('./filelist.js');
+const customLoader = require('./customLoader.js');
 
-module.exports = {
-    context: path.resolve(__dirname,"../app"), // __dirname - текущая папка, корневой модуль
-    entry: {
-        babel_polyfill: 'babel-polyfill', // name: path, to get async/await working
-        main: "./scripts/main",
-        vendor: Object.keys(package.dependencies),
-        settings: "./scripts/settings"
-    }, // корневой элемент
-    output: {
-        path: path.resolve(__dirname,"../dist"), // все сформированные ассеты
-        //publicPath: 'dist/',
-        filename: "[name].bundle.js",
-        chunkFilename: '[name].bundle.js'
-    },
-    resolve: {
-        // alias: { xyz: "/absolute/path/to/file.js" }, // require("xyz"); где искать модули, для сокращения путей
-        mainFiles: ["index"], // дефолтный файл в импортируемой папке
-        modules: ["web_modules", "node_modules"], // где искать модули без относительного пути
-        extensions: [".js", ".jsx", ".json", ".scss"]
-    },
-    resolveLoader: { // для комплиляции
-        modules: ["web_loaders", "web_modules", "node_modules"],
-        extensions: [".js", ".json"]
-    },
-    devServer: { // the browser could refresh by itself
-        contentBase: path.join(__dirname, "../dist/"), // base folder
-        port: 9000 // on what port the localhost server is to be spun
-    },
-    module: {
-        rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader", options: {
-                "presets": ["env"],
-                "plugins": [
-                  //"./plugin.js", // выключать на DEV
-                  "syntax-dynamic-import"
-                ]
-              }
-            },
-            {
-                test: /\.(s*)css$/,
-                use: ExtractTextPlugin.extract({ // выделить CSS в отдельный файл
-                    fallback:'style-loader', // 3) do the job of putting the css codes inside <style> tags in our index.html
-                    use:[ // цепочка из нескольких лоадеров
+module.exports = (env = {}) => {
+    const isProd = env === 'prod';
+    return {
+        context: path.resolve(__dirname,"../app"), // __dirname - текущая папка, корневой модуль
+        entry: {
+            babel_polyfill: 'babel-polyfill', // name: path, to get async/await working
+            main: "./scripts/main",
+            vendor: Object.keys(package.dependencies),
+            settings: "./scripts/settings"
+        }, // корневой элемент
+        output: {
+            path: path.resolve(__dirname,"../dist"), // все сформированные ассеты
+            //publicPath: 'dist/',
+            filename: "[name].bundle.js",
+            chunkFilename: '[name].bundle.js'
+        },
+        resolve: {
+            // alias: { xyz: "/absolute/path/to/file.js" }, // require("xyz"); где искать модули, для сокращения путей
+            mainFiles: ["index"], // дефолтный файл в импортируемой папке
+            modules: ["web_modules", "node_modules"], // где искать модули без относительного пути
+            extensions: [".js", ".jsx", ".json", ".scss"]
+        },
+        resolveLoader: { // для комплиляции
+            modules: ["web_loaders", "web_modules", "node_modules"],
+            extensions: [".js", ".json"]
+        },
+        devServer: { // the browser could refresh by itself, сборка происходит в памяти
+            contentBase: path.join(__dirname, "../dist/"), // base folder
+            port: 9000, // on what port the localhost server is to be spun
+            hot: true, // без обновления страницы, hot module replacement
+            inline: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader",
+                    options: {
+                        "presets": ["env"],
+                        "plugins": [
+                        //"./plugin.js", // выключать на DEV
+                        "syntax-dynamic-import"
+                        ]
+                }
+                },
+                {
+                    test: /\.(s*)css$/,
+                    use: ExtractTextPlugin.extract({ // выделить CSS в отдельный файл
+                        fallback:'style-loader', // 3) do the job of putting the css codes inside <style> tags in our index.html
+                        use:[ // цепочка из нескольких лоадеров
+                            {
+                                loader: 'css-loader', // 2)
+                                options: {}
+                            },
+                            {
+                                loader: 'postcss-loader', // плагин PostCSS для добавления вендорных префиксов на основе данных сервиса Can I Use
+                                options: {
+                                    plugins: [
+                                        autoprefixer({
+                                            browsers:['ie >= 8', 'last 4 version']
+                                        })
+                                    ],
+                                    sourceMap: true
+                                }
+                            },
+                            {
+                                loader: 'sass-loader', // 1) преобразование из sass в css
+                                options: {}
+                            }
+                        ],
+                    })
+                },
+                {
+                    test: /\.(png|jp(e*)g|svg)$/,  
+                    use: [{
+                        loader: 'url-loader',
+                        options: { 
+                            limit: 20000, // Convert images < 20kb to base64 strings
+                            name: isProd ? 'images/[name].[ext]' : 'images/[name]_[hash].[ext]'
+                        } 
+                    }]
+                },
+                {
+                    test: /\.json$/,
+                    exclude: /node_modules/,
+                    use: [
                         {
-                            loader: 'css-loader', // 2)
-                            options: {}
+                            loader: 'file-loader',
+                            options: {name: 'assets/[name].[ext]'}
                         },
                         {
-                            loader: 'sass-loader', // 1) преобразование из sass в css
-                            options: {}
+                            loader: '../webpack/customLoader',
                         }
-                    ],
-                })
-            },
-            {
-                test: /\.(png|jp(e*)g|svg)$/,  
-                use: [{
-                    loader: 'url-loader',
-                    options: { 
-                        limit: 20000, // Convert images < 20kb to base64 strings
-                        name: 'images/[hash]-[name].[ext]'
-                    } 
-                }]
-            }
+                    ]
+                }
+            ]
+        },
+        devtool: isProd ? "source-map" : "eval", // для DEV, собирается быстро
+        // devtool: "source-map", // для PROD, собирается медленно
+        // watch: true, // пересборка после изменений, when we added devServer, we don’t need to specify watch:true
+        watchOptions: {
+            aggregateTimeout: 300,
+            ignored: /node_modules/
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                ENVIRONMENT: 'development'
+            }),
+            new webpack.optimize.CommonsChunkPlugin({ // общие для всез модули
+                name: 'shared', // new file-name
+                minChunks: Infinity // create a chunk if the module is found to be shared in at-least two other files/modules
+            }),
+            new HtmlWebpackPlugin({ // преобразование html
+                hash: true, // add a unique hash to the src of the embedded <script> tag
+                title: 'News API', // custom property
+                template: 'index.html', // which file to use as template for the index.html being created
+                chunks: ['babel_polyfill', 'vendor', 'shared', 'main'],
+                filename: 'index.html' //relative to root of the application
+            }),
+            new HtmlWebpackPlugin({ // преобразование html
+                hash: true, // add a unique hash to the src of the embedded <script> tag
+                title: 'News API', // custom property
+                template: 'index.html', // which file to use as template for the index.html being created
+                chunks: ['babel_polyfill', 'vendor', 'shared', 'settings'],
+                filename: 'settings.html' //relative to root of the application
+            }),
+            new ExtractTextPlugin({filename:'app.bundle.css'}),
+            new CopyWebpackPlugin([ //  копирование файлов из начальной папки в конечную
+                {from:'images',to:'images'} //, {from:'assets',to:'assets'} 
+            ]),
+            new webpack.HotModuleReplacementPlugin(),
+            new filelist()
         ]
-    },
-    devtool: "eval", // для DEV, собирается быстро
-    // devtool: "source-map", // для PROD, собирается медленно
-    // watch: true, // пересборка после изменений, when we added devServer, we don’t need to specify watch:true
-    watchOptions: {
-        aggregateTimeout: 300,
-        ignored: /node_modules/
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            ENVIRONMENT: 'development'
-        }),
-        new webpack.optimize.CommonsChunkPlugin({ // общие для всез модули
-            name: 'shared', // new file-name
-            minChunks: Infinity // create a chunk if the module is found to be shared in at-least two other files/modules
-        }),
-        new HtmlWebpackPlugin({ // преобразование html
-            hash: true, // add a unique hash to the src of the embedded <script> tag
-            title: 'News API', // custom property
-            template: 'index.html', // which file to use as template for the index.html being created
-            chunks: ['babel_polyfill', 'vendor', 'shared', 'main'],
-            filename: 'index.html' //relative to root of the application
-        }),
-        new HtmlWebpackPlugin({ // преобразование html
-            hash: true, // add a unique hash to the src of the embedded <script> tag
-            title: 'News API', // custom property
-            template: 'index.html', // which file to use as template for the index.html being created
-            chunks: ['babel_polyfill', 'vendor', 'shared', 'settings'],
-            filename: 'settings.html' //relative to root of the application
-        }),
-        new ExtractTextPlugin({filename:'app.bundle.css'}),
-        new CopyWebpackPlugin([ //  копирование файлов из начальной папки в конечную
-            {from:'images',to:'images'} 
-        ])
-    ]
+    }
 }
