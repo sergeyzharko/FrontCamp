8d61269d2ff15f6569061f27639ba03f7f44eb5a
sergeyzharko <sergeyzharko@gmail.com>
2018-03-01 19:48:29 +0300

    Prototype

--- app/scripts/sources.js
+++ app/scripts/sources.js
@@ -1,36 +1,17 @@
 import '../assets/sources.json';
-var sources;
 var url = 'https://newsapi.org/v2/sources?' +
 'apiKey=f53b56a81c57478689c3058487c41269';
 
-var req = new Request(url);
-
-fetch(req)
-.then(function(response) {
-    console.log(response.json());
-})
-
-
-// Async/Await - задание со звёздочкой
-async function readSourceArray(){
-    return await sources;
-}
-
-export default function loadSources() {
-
-    fetch('./assets/sources.json')
-    .then(function(response) {
-      console.log(response.headers.get('Content-Type')); // application/json; charset=utf-8
-      console.log(response.status); // 200
-    
-      return response.json();
-     })
-    .then(function(res) {
-      sources = res;
-      let buttons = document.querySelector('.buttons');
-      readSourceArray().then(
-        (sources => {
-            sources.forEach( source => {
+export default class sources {
+    constructor(name) {
+        this.sources;
+      }
+      async loadSources() {
+        this.sources = await fetch('./assets/sources.json');
+      }
+      renderSources() {
+        let buttons = document.querySelector('.buttons');
+        this.sources.json().then( src => src.forEach( source => {
                 let button = document.createElement('div');
                 button.classList.add('source');
                 button.classList.add(source.link);
@@ -39,16 +20,14 @@ export default function loadSources() {
                     <a href="#"><div class="picture lion"></div></a>`;
                 // button.addEventListener('click', () => loadNews(source.link) );
                 button.addEventListener('click', () => import(/* webpackChunkName: "news" */ './news').then(module => {
-                        var loadNews = module.default;
-                        loadNews(source.link);
+                        let news = module.default;
+                        let block = new news(source.link);
+                        block.loadNews().then( () => block.renderNews() );
                     })
                 );
                 buttons.appendChild(button);
-            });
-        }) 
-      )
-    })
-    .catch( console.log );
+            })
+        );
 
+      }
 }
-
